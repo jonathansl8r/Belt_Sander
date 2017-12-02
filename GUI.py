9db8645a788bf015364c4601b49041f8cfaf4f6e
Jonathan <jonathansl8r@gmail.com>
2017-12-02 14:10:23 -0800

    Corrected scroll logic

--- GUI.py
+++ GUI.py
@@ -6,36 +6,47 @@ import pigpio
 class gui:
     def __init__(self, pi, left, right, up, down, select):
         self.pi = pi
-        self.run_display = [" Sand Speed: ", " Belt Speed: ", " Thickness: ", " Direction: ", " Home Belt"]
+        self.run_display = [" Sand Speed:", " Belt Speed:", " Thickness:", " Direction:", " Home Belt"]
         self.left = left
         self.right = right
         self.up = up
         self.down = down
         self.select = select
-        self.cursor = [0, 0]
+        self.cursor = 0
         self.state = ""
-        self.triggered = False
         self.lcd = RPi_I2C_driver.lcd()
 
     def welcome(self):
         self.lcd.lcd_display_string("---- SANDER ----", 1)
         self.lcd.lcd_display_string("RasPi Zero W", 2)
-        time.sleep(.2)
+        time.sleep(.5)
         self.lcd.lcd_clear()
 
-    def disp_run_display(self, i):
-        self.lcd.lcd_display_string(self.run_display[i], 1)
-        self.lcd.lcd_display_string(self.run_display[i + 1], 2)
-        self.cursor = i
+    def screen_home(self, screen):
+        self.cursor = 0
+        self.lcd.lcd_display_string(screen[0], 1)
+        self.lcd.lcd_display_string(screen[1], 2)
 
     def scroll(self, screen, dir):
-        if  self.cursor[0] == 0 and dir == -1:
-            self.cursor[0] = 1
-        elif self.cursor[0] == 1 and dir == -1:
-            if len(screen) < self.cursor[0]+1:
-                self.cursor[0] = 0
-            else:
-                self.cursor[0] += 1
+        # Get new cursor index and print new screens
+        if self.cursor == len(screen) - 1 and dir == -1:
+            self.cursor = 0
+            s = [">" + screen[self.cursor][1:], screen[self.cursor + 1]]
+        elif dir == -1:
+            self.cursor -= dir
+            s = [screen[self.cursor - 1], ">" + screen[self.cursor][1:]]
+        elif self.cursor == 0 and dir == 1:
+            self.cursor = len(screen) - 1
+            s = [screen[self.cursor - 1], ">" + screen[self.cursor][1:]]
+        elif dir == 1:
+            self.cursor -= dir
+            s = [">" + screen[self.cursor][1:], screen[self.cursor + 1]]
+        else:
+            s = None
+
+        if not s == None:
+            self.lcd.lcd_display_string(s[0], 1)
+            self.lcd.lcd_display_string(s[0], 2)
 
     def arrow_cursor(self, screen, i):
         pass
@@ -44,9 +55,8 @@ class gui:
         pass
 
     def run(self):
-        lcd = RPi_I2C_driver.lcd()
         self.welcome()
-        self.disp_run_display(self.lcd, 0)
+        self.screen_home(self.run_display)
         self.cb_left = self.pi.callback(self.left, pigpio.RISING_EDGE, self._cbf)
         self.cb_right = self.pi.callback(self.right, pigpio.RISING_EDGE, self._cbf)
         self.cb_up = self.pi.callback(self.up, pigpio.RISING_EDGE, self._cbf)
